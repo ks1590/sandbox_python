95d549ed06e39d53388b2f6ac0d4134a2b5f2f37
ks1590 <69412467+ks1590@users.noreply.github.com>
2022-09-08 23:56:49 +0900

    wip add: login function

--- base/views.py
+++ base/views.py
@@ -1,10 +1,35 @@
 from multiprocessing import context
 from re import I
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib import messages
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist')
+
+
+    coontext = {}
+    return render(request, 'base/login_register.html', coontext)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
