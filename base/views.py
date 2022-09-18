630355b45fce663af71dcc0a8a9a2de3b6b5e81f
ks1590 <69412467+ks1590@users.noreply.github.com>
2022-09-18 23:56:47 +0900

    add: user registration

--- base/views.py
+++ base/views.py
@@ -7,12 +7,14 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 def loginPage(request):
+    page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -29,12 +31,27 @@ def loginPage(request):
             messages.error(request, 'Username or Password does not exist')
 
 
-    coontext = {}
+    coontext = {'page': page}
     return render(request, 'base/login_register.html', coontext)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during redigtration')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
