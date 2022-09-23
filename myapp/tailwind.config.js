b4402be9023f93c984e9a14b89f4406eb455dc7d
ks1590 <69412467+ks1590@users.noreply.github.com>
2022-09-24 00:05:16 +0900

    install Flowbite

--- myapp/tailwind.config.js
+++ myapp/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/**/*.html'
+      './templates/**/*.html',
+      './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin')
+  ],
 }
